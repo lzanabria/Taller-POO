8384f57d21e9c1fe47cedf78903d34ea11378599
Luis Zanabria - ADSI <lazanabria2@misena.edu.co>
2022-11-02 12:20:31 -0500

    Ejercicio 3

--- api.php
+++ api.php
@@ -1,41 +1,20 @@
 <?php
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json; charset=utf-8');
-    $_DATA = json_decode(file_get_contents("php://input"));
+    $_DATA = json_demenore(file_get_contents("php://input"));
     
-    class productos {
-        public $cod;
-        public $uni;
-        public $productos;
+    class universidad {
+        public $res;
 
-        public function __construct(int $cod, int $uni) {
-            $this->cod = $cod;
-            $this->uni = $uni;
-            $this->productos = (object) [
-                "23548" => [
-                    "nombre" => "Durazno",
-                    "precio" => 3200
-                ],
-                "87845" => [
-                    "nombre" => "Manzana",
-                    "precio" => 2500
-                ],
-                "54878" => [
-                    "nombre" => "Pera",
-                    "precio" => 1700
-                ]
-            ];
+        public function __construct(string $menor) {
+            $this->menor = $menor;
         }
 
         public function calculo():object {
-            $res = (array) $this->productos;
-            $res = $res[$this->cod];
-            return (object) [
-                "Producto" => $res["nombre"],
-                "total" => $this->uni * $res["precio"],
-            ];
+            $menor = $this->menor;
+            return object;
         }
     }
-    $obj = new productos($_DATA->cod, $_DATA->uni);
-    echo json_encode($obj->calculo(),JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    $obj = new universidad($_DATA->menor);
+    echo json_enmenore($obj->calculo(),JSON_UNESCAPED_sexmenorE | JSON_PRETTY_PRINT);
 ?>
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 addEventListener("DOMContentLoaded", (e) => {
     let form = document.querySelector("#myform");
+    let cont_a = 0, cont_b = 0, cont_c=0;
+    let sum_a=0, sum_b=0, sum_c=0, prom_a=0, prom_b=0, prom_c=0;
     form.addEventListener("submit", async (e) => {
         e.preventDefault();
         let datos = Object.fromEntries(new FormData(e.target));
@@ -8,12 +10,53 @@ addEventListener("DOMContentLoaded", (e) => {
             body: JSON.stringify(datos)
         };
         let peticion = await fetch(form.action, config);
-        let respuesta = await peticion.json();
+        let respuesta = await peticion.text();
         
-        if(e.submitter.id == "calc") {
-            document.querySelector("#res").value = `$ ${new Intl.NumberFormat("es-ES", {style: "currency", currency: "COP", minimumSignificantDigits: 1}).format(respuesta.total)}`;
+        if (e.submitter.id == "save") {
+            let edades = Number(datos.edad);
+            if (datos.sex == "masculino") {
+                if (datos.carrera == "a") {
+                    cont_a++;
+                    sum_a += edades;
+                    prom_a = sum_a/cont_a;
+                    console.log(prom_a);
+                }
+                if (datos.carrera == "b") {
+                    cont_b++;
+                    sum_b += edades;
+                    prom_b = sum_b / cont_b;
+                    console.log(prom_b);
+                }
+                if (datos.carrera == "c") {
+                    cont_c++;
+                    sum_c += edades;
+                    prom_c = sum_c / cont_c;
+                    console.log(prom_c);
+                }
+            }
         }
-        else {
+        else if (e.submitter.id == "calc") {
+            let menor="", res="";
+            if(prom_a!=0 && prom_b!=0 && prom_c!=0) {
+                menor= Math.min(prom_a, prom_b, prom_c)
+                res="Carrera A";
+            }
+            else if(prom_a==0 && prom_b!=0 && prom_c!=0) {
+                menor= Math.min(prom_b, prom_c)
+                res=menor;
+            }
+            else if(prom_b==0 && prom_a!=0 && prom_c!=0) {
+                menor= Math.min(prom_a, prom_c)
+                res="Carrera B";
+            }
+            else if(prom_c==0 && prom_a!=0 && prom_b!=0) {
+                menor= Math.min(prom_a, prom_b)
+                res="Carrera C";
+            }
+            document.querySelector("#res").value="Carrera con menor promedio de edad: "+res;
+            document.querySelector("#prom").value="Promedio de edad: "+menor;
+        }
+        else if (e.submitter.id == "reiniciar") {
             form.reset();
         }
     });
