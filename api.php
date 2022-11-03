0f989bd42314091d8b2a55dce4f396640329efaa
Luis Zanabria - ADSI <lazanabria2@misena.edu.co>
2022-11-03 12:10:14 -0500

    Ejercicio 5

--- api.php
+++ api.php
@@ -1,26 +1,75 @@
 <?php
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json; charset=utf-8');
-    
-    class pago {
-        public $mens=0;
-        public $total=0;
-        public function calculo():array {
-            $men = [];
-            for($i=1; $i<=20; $i++) {
-                if($i==1) {
-                    $this->mens = 10;
-                }
-                else {
-                    $this->mens *=2;
-                }
-                $this->total += $this->mens;
-                array_push($men, ["mensaje" => "El pago del mes $i es de $: $this->mens"]);
+    $_DATA = json_decode(file_get_contents("php://input"));
+
+    class ventas { 
+        public $venta;
+        public $global=0;
+        public $listaA;
+        
+        public function __construct(int $venta) {
+            if($venta){
+                $this->venta = $venta;
+                $data = json_decode(file_get_contents("Ventas.json"), true);
+                array_push($data,(object) [
+                "Ventas" => [
+                    "Monto" => $this->venta,
+                    ]
+                ]);
+                $file = fopen("Ventas.json", "w+");
+                fwrite($file, json_encode($data, JSON_PRETTY_PRINT));
+                fclose($file);
             }
-            array_push($men, ["total" => "El pago total es de: $: $this->total"]);
-            return $men;
-        }  
+        }
+
+        public function calculo() {
+            $data = json_decode(file_get_contents("Ventas.json"), true);
+            $this->global = 0;
+            foreach ($data as $key => $value) {
+                $this->global += $value["Ventas"]["Monto"];
+            }
+            $lista = array_column(array_column($data, 'Ventas'), "Monto");
+            $categoriaA = array_filter($lista, function($item) {
+                return $item > 1000;
+            });
+
+            $sumaA = array_sum($categoriaA);
+
+            $totalA = count($categoriaA);
+            $this->listaA = (object) [
+                "CategoriaA" => [
+                    "n_items" => $totalA,
+                    "Monto" => $sumaA,
+                    "Ventas"=> $categoriaA
+                ],
+            ];
+
+            $categoriaB = array_filter($lista, function($item) {
+                return $item >500 && $item <= 1000;
+            });
+            $sumaB = array_sum($categoriaB);
+            $totalB = count($categoriaB);
+            $this->listaA->CategoriaB = [
+                "n_items" => $totalB,
+                "Monto" => $sumaB,
+                "Ventas"=> $categoriaB
+            ];
+
+            $categoriaC = array_filter($lista, function($item) {
+                return $item <=500;
+            });
+            $sumaC = array_sum($categoriaC);
+            $totalC = count($categoriaC);
+            $this->listaA->CategoriaC = [
+                "n_items" => $totalC,
+                "Monto" => $sumaC,
+                "Ventas"=> $categoriaC
+            ];
+            $this->listaA->global = $this->global;
+            return $this->listaA;
+        }
     }
-    $obj = new pago();
-    echo json_encode($obj->calculo(), JSON_PRETTY_PRINT);
+    $obj = new ventas($_DATA->venta);
+    echo json_encode($obj->calculo(),JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
 ?>
--- main.js
+++ main.js
@@ -1,22 +1,36 @@
 addEventListener("DOMContentLoaded", (e) => {
     let form = document.querySelector("#myform");
-    form.addEventListener("submit", async (e) => {
+    form.addEventListener("submit", async(e) => {
         e.preventDefault();
-
-        let peticion = await fetch(form.action);
-        let respuesta = await peticion.json();
-    
-        if (e.submitter.id == "calc") {
-            respuesta.forEach((element, id) => {
-                if(id==20){
-                    document.querySelector("#res").value += "\n"+element.total;
-                }else{
-                    document.querySelector("#res").value += element.mensaje+"\n";
-                }
-            });
+        let data = Object.fromEntries(new FormData(e.target));
+        
+        if(e.submitter.id == "save") {
+            let config = {
+                method: form.method,
+                body: JSON.stringify(data)
+            };
+            let peticion = await fetch(form.action, config);
+            let respuesta = await peticion.json();
+            document.querySelector(`[name="venta"]`).value="0";
         }
+
+        else if (e.submitter.id == "calc") {
+            data.venta = 0;
+            let config = {
+                method: form.method,
+                body: JSON.stringify(data)
+            };
+            let peticion = await fetch(form.action, config);
+            let respuesta = await peticion.text();
+            console.log(respuesta);
+            document.querySelector("#res").value = respuesta;
+        }
+
         else if (e.submitter.id == "reiniciar") {
             form.reset();
         }
+    });
+    document.querySelector(`[name="venta"]`).addEventListener("click", (e) => {
+        e.target.value = "";
     });
 });
