149ca58525b271f4422d77944443a87227776a72
Luis Zanabria - ADSI <lazanabria2@misena.edu.co>
2022-11-03 12:26:12 -0500

    Ejercicio 6

--- api.php
+++ api.php
@@ -3,73 +3,21 @@
     header('Content-Type: application/json; charset=utf-8');
     $_DATA = json_decode(file_get_contents("php://input"));
 
-    class ventas { 
-        public $venta;
-        public $global=0;
-        public $listaA;
+    class taller { 
+        public $presion;
+        public $volumen;
+        public $masa;
         
-        public function __construct(int $venta) {
-            if($venta){
-                $this->venta = $venta;
-                $data = json_decode(file_get_contents("Ventas.json"), true);
-                array_push($data,(object) [
-                "Ventas" => [
-                    "Monto" => $this->venta,
-                    ]
-                ]);
-                $file = fopen("Ventas.json", "w+");
-                fwrite($file, json_encode($data, JSON_PRETTY_PRINT));
-                fclose($file);
-            }
+        public function __construct(int $presion, int $volumen) {
+            $this->presion = $presion;
+            $this->volumen = $volumen;
         }
 
         public function calculo() {
-            $data = json_decode(file_get_contents("Ventas.json"), true);
-            $this->global = 0;
-            foreach ($data as $key => $value) {
-                $this->global += $value["Ventas"]["Monto"];
-            }
-            $lista = array_column(array_column($data, 'Ventas'), "Monto");
-            $categoriaA = array_filter($lista, function($item) {
-                return $item > 1000;
-            });
-
-            $sumaA = array_sum($categoriaA);
-
-            $totalA = count($categoriaA);
-            $this->listaA = (object) [
-                "CategoriaA" => [
-                    "n_items" => $totalA,
-                    "Monto" => $sumaA,
-                    "Ventas"=> $categoriaA
-                ],
-            ];
-
-            $categoriaB = array_filter($lista, function($item) {
-                return $item >500 && $item <= 1000;
-            });
-            $sumaB = array_sum($categoriaB);
-            $totalB = count($categoriaB);
-            $this->listaA->CategoriaB = [
-                "n_items" => $totalB,
-                "Monto" => $sumaB,
-                "Ventas"=> $categoriaB
-            ];
-
-            $categoriaC = array_filter($lista, function($item) {
-                return $item <=500;
-            });
-            $sumaC = array_sum($categoriaC);
-            $totalC = count($categoriaC);
-            $this->listaA->CategoriaC = [
-                "n_items" => $totalC,
-                "Monto" => $sumaC,
-                "Ventas"=> $categoriaC
-            ];
-            $this->listaA->global = $this->global;
-            return $this->listaA;
+            $this->masa = $this->presion * $this->volumen;
+            return $this->masa;
         }
     }
-    $obj = new ventas($_DATA->venta);
+    $obj = new taller ($_DATA->presion, $_DATA->volumen);
     echo json_encode($obj->calculo(),JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
 ?>
--- main.js
+++ main.js
@@ -3,34 +3,19 @@ addEventListener("DOMContentLoaded", (e) => {
     form.addEventListener("submit", async(e) => {
         e.preventDefault();
         let data = Object.fromEntries(new FormData(e.target));
-        
-        if(e.submitter.id == "save") {
-            let config = {
-                method: form.method,
-                body: JSON.stringify(data)
-            };
-            let peticion = await fetch(form.action, config);
-            let respuesta = await peticion.json();
-            document.querySelector(`[name="venta"]`).value="0";
-        }
+        let config = {
+            method: form.method,
+            body: JSON.stringify(data)
+        };
+        let peticion = await fetch(form.action, config);
+        let respuesta = await peticion.json();
 
-        else if (e.submitter.id == "calc") {
-            data.venta = 0;
-            let config = {
-                method: form.method,
-                body: JSON.stringify(data)
-            };
-            let peticion = await fetch(form.action, config);
-            let respuesta = await peticion.text();
-            console.log(respuesta);
-            document.querySelector("#res").value = respuesta;
+        if (e.submitter.id == "calc") {
+            document.querySelector("#res").value = "La masa de aire de la rueda es: "+respuesta;
         }
 
         else if (e.submitter.id == "reiniciar") {
             form.reset();
         }
-    });
-    document.querySelector(`[name="venta"]`).addEventListener("click", (e) => {
-        e.target.value = "";
     });
 });
